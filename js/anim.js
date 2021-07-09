e1d3bec6be37b53d4912346bd6728280d27cb86e
gsusha <x.dyachenko@gmail.com>
2021-07-09 19:50:09 +0300

    almost ready 0.2

--- js/anim.js
+++ js/anim.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
 $(".home__img").animate({top: "0", left: "0"}, 1500);
-$(".home__text").animate({top: "0", left: "0"}, 1500);
+$(".home__text").animate({left: "0"}, 1500);
 });
